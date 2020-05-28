285995fb50c1df9208613279cb9d0ceb1c50d17f
amir-alaj <56620811+amir-alaj@users.noreply.github.com>
2020-05-27 18:20:13 -0700

    Muzzle flare additions

--- FinalProject/Assets/PBR Weapons Pack/AK74M/GunController.cs
+++ FinalProject/Assets/PBR Weapons Pack/AK74M/GunController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GunController : MonoBehaviour
 {
     Animator m_animator;
+
     void Start()
     {
         m_animator = GetComponent<Animator>();
@@ -12,6 +13,7 @@ public class GunController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
             m_animator.SetTrigger("Shoot");
